505ac80c59f2d7b1b63a2d909a89286198c78a9d
Chris Léon <chris@dev.wazo.lu>
2024-10-17 10:27:28 +0400

    Add unit test repository

---
+++ BlogTest/BlogRepositoryTests.cs
@@ -0,0 +1,78 @@
+﻿
+
+using Blogs.Entities;
+using Blogs.Repositories;
+using Moq;
+
+namespace BlogTest;
+
+public class BlogRepositoryTests
+{
+    private readonly Mock<IBlogReposiroty> _blogRepositoryMock;
+    private readonly CancellationToken _cancellationToken;
+
+    public BlogRepositoryTests()
+    {
+        _blogRepositoryMock = new Mock<IBlogReposiroty>();
+        _cancellationToken = new CancellationToken();
+    }
+
+    [Fact]
+    public async Task Create_ShouldReturnGuid_WhenBlogIsCreated()
+    {
+        
+        var blog = new Blog { /* initialize properties */ };
+        var blogId = Guid.NewGuid();
+        _blogRepositoryMock.Setup(repo => repo.Create(blog, _cancellationToken)).ReturnsAsync(blogId);
+
+        
+        var result = await _blogRepositoryMock.Object.Create(blog, _cancellationToken);
+
+        
+        Assert.Equal(blogId, result);
+    }
+
+    [Fact]
+    public async Task Update_ShouldReturnGuid_WhenBlogIsUpdated()
+    {
+        
+        var blog = new Blog { /* initialize properties */ };
+        var blogId = Guid.NewGuid();
+        _blogRepositoryMock.Setup(repo => repo.Udpdate(blog, _cancellationToken)).ReturnsAsync(blogId);
+
+     
+        var result = await _blogRepositoryMock.Object.Udpdate(blog, _cancellationToken);
+
+      
+        Assert.Equal(blogId, result);
+    }
+
+    [Fact]
+    public async Task Get_ShouldReturnBlog_WhenIdIsProvided()
+    {
+        
+        var blogId = Guid.NewGuid();
+        var blog = new Blog { /* initialize properties */ };
+        _blogRepositoryMock.Setup(repo => repo.Get(blogId, _cancellationToken)).ReturnsAsync(blog);
+
+        
+        var result = await _blogRepositoryMock.Object.Get(blogId, _cancellationToken);
+
+        
+        Assert.Equal(blog, result);
+    }
+
+    [Fact]
+    public async Task Delete_ShouldReturnGuid_WhenBlogIsDeleted()
+    {
+        
+        var blogId = Guid.NewGuid();
+        _blogRepositoryMock.Setup(repo => repo.Delete(blogId, _cancellationToken)).ReturnsAsync(blogId);
+
+      
+        var result = await _blogRepositoryMock.Object.Delete(blogId, _cancellationToken);
+
+  
+        Assert.Equal(blogId, result);
+    }
+}
--- Blogs/Repositories/IBlogReposiroty.cs
+++ Blogs/Repositories/IBlogReposiroty.cs
@@ -1,12 +1,11 @@
 ﻿using Blogs.Entities;
 
-namespace Blogs.Repositories
+namespace Blogs.Repositories;
+
+public interface IBlogReposiroty
 {
-    public interface IBlogReposiroty
-    {
-        Task<Guid> Create(Blog blogs, CancellationToken cancellationToken);
-        Task<Guid> Udpdate(Blog blogs, CancellationToken cancellationToken);
-        Task<Blog> Get(Guid id, CancellationToken cancellationToken);
-        Task<Guid> Delete(Guid id, CancellationToken cancellationToken);
-    }
+    Task<Guid> Create(Blog blogs, CancellationToken cancellationToken);
+    Task<Guid> Udpdate(Blog blogs, CancellationToken cancellationToken);
+    Task<Blog> Get(Guid id, CancellationToken cancellationToken);
+    Task<Guid> Delete(Guid id, CancellationToken cancellationToken);
 }
